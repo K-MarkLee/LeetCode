86d58a4ac8e4ac318700bf986e18153d81910568
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2024-11-15 09:24:57 +0900

    Time: 1361 ms (47.45%), Space: 0B (100.00%) - LeetHub

---
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select
+case 
+    when e.id is not null 
+    then u.unique_id
+    else null
+end as unique_id,
+e.name
+from Employees e
+left join EmployeeUNI u
+on e.id = u.id
